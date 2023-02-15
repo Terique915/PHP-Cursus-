55b31438c778accbe1d02e2b48053710f290174f
Terique915 <54835492+Terique915@users.noreply.github.com>
2023-02-15 12:41:42 +0100

    opdracht verbetering

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Homepage</title>
+
+</head>
+<body>
+<h1>PHP Cursus</h1>
+<h2>Terique Blijden</h2>
+<div>
+    <ul>
+        <li><a href="H01/helloworld.php">Hoofdstuk 1- Hello World</a> </li>
+
+
+
+
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 2</h2>
+        <li><a href="H02/zinnen.php">Opdracht 1 - zin 1</a> </li>
+        <li><a href="H02/zinnen2.php">Opdracht 2 - zin 2</a> </li>
+        <li><a href="H02/zinnen3.php">Opdracht 3 - zin 3 </a> </li>
+        <li><a href="H02/zinnen4.php">OPdracht 4 - zin 4</a> </li>
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 3</h2>
+        <li><a href="H03/Opdracht%201.php">Opdracht 1- Controle structuren en loops</a> </li>
+        <li><a href="H03/Opdracht2.php">Opdracht 2- Kerstboom</a> </li>
+        <li><a href="H03/Opdracht3.php">Opdracht 3- van while naar for-loop</a> </li>
+        <li><a href="H03/Opdracht4.php">Opdracht 4- Loop en controlstructuur</a> </li>
+        <li><a href="H03/Opdracht5.php">Opdracht 5- Busreis</a> </li>
+        <li><a href="H03/Opdracht6.php">Opdracht 6- zwemclubs</a> </li>
+        <li><a href="H03/Opdracht7.php">Opdracht 7- Kappersageda</a> </li>
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 4</h2>
+        <li><a href="H04/Opdracht1.php">Opdracht 4</a> </li>
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 5</h2>
+        <li><a href="H05/Opdracht1.html">Opdracht 1- Dyanmische Website</a> </li>
+        <li><a href="H05/Formulier.html">Opdracht 2-</a> </li>
+        <li><a href="H05/Animalselect.html">Opdracht 3</a> </li>
+        <li><a href="H05/Beasties%20Guru.html">Opdracht 4</a> </li>
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 6</h2>
+        <li><a href="H06/Cursisten.php">Cursisten</a></li>
+        <li><a href="H06/LoginDB.php" >Login met Database</a></li>
+    </ul>
+    <ul>
+        <h2>Hoofdstuk 7</h2>
+        <li><a href="H07/Login.php">Cookies & Sessions</a></li>
+
+
+    </ul>
+
+</div>
+</body>
+</html>
